e0c81fb83ab4411cc8556cb4dab70be8c13c3ed3
Markus Bader <markus.bader@mx-robotics.com>
2022-12-13 12:03:34 +0100

    x

--- include/stage_ros2/stage_node.hpp
+++ include/stage_ros2/stage_node.hpp
@@ -19,8 +19,13 @@ private:
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
 
+  double value_double;
+  int value_int;
   // The main simulator object
   Stg::World* world;
+    rclcpp::Service<StageNode::srv::ControlParameter>::SharedPtr srv_control_param_request_;
+  void init_parameter();
+  void callback_update_parameter();
 };
 
 #endif  // STAGE_ROS2_PKG__STAGE_ROS_HPP_
--- src/stage_node.cpp
+++ src/stage_node.cpp
@@ -1,12 +1,16 @@
 #include <chrono>
 
 #include "stage_ros2/stage_node.hpp"
+#include <sys/stat.h>
 
 using namespace std::chrono_literals;
 
 StageNode::StageNode(rclcpp::NodeOptions options)
 : Node("publisher_node", options), count_(0)
 {
+    
+    srv_control_param_request_ = this->create_service<mxr_msgs::srv::ControlParameter>("control_parameters", std::bind(&MXRBaseNode::callback_srv_control_parameter, this, _1, _2));
+
   publisher_ = create_publisher<std_msgs::msg::String>("topic", 10);
   timer_ = create_wall_timer(
     500ms, std::bind(&StageNode::on_timer, this));
@@ -20,6 +24,32 @@ void StageNode::on_timer()
   publisher_->publish(message);
 }
 
+
+void StageNode::callback_update_parameter()
+{
+    
+    this->get_parameter("accelleration", value_double);
+    this->get_parameter("value_int", value_int);
+    
+    if(count_callback_joy_ == 0) RCLCPP_INFO(this->get_logger(), "no joy message received");
+}
+
+void StageNode::init_parameter() {
+    
+    this->declare_parameter<double>("value_double", value_double);
+    
+    {
+        rcl_interfaces::msg::ParameterDescriptor descriptor;
+        rcl_interfaces::msg::IntegerRange range;
+        range.set__from_value(0).set__to_value(100).set__step(1);
+        descriptor.integer_range= {range};
+        this->declare_parameter("value_int", 1, descriptor);
+    }
+    
+    
+    callback_update_parameter();
+    timer_ = this->create_wall_timer(1000ms, std::bind(&StageNode::callback_update_parameter, this));
+}
 
 void StageNode::init(int argc, char** argv, bool gui, const char* fname, bool use_model_names){
 
